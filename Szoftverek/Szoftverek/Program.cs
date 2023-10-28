24f3a13b65f262c72a63fabe8ba9ef73cab6ac54
Orosz Péter <70895314+Petiiiiiiiiii@users.noreply.github.com>
2023-10-28 15:24:54 +0200

    Add files via upload

---
+++ Szoftverek/Szoftverek/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Text;
+using Szoftverek;
+
+static List<Szoftver> MagasErtekelesuAntivirus(List<Szoftver> szoftverek) 
+{
+    return szoftverek
+        .Where(sz => sz.Ertekeles > 8.5 && sz.Kategoria == "antivírus")
+        .ToList();
+}
+static void MajdnemLegjobbErtekeles(List<Szoftver> szoftverek) 
+{
+    double legjobb = szoftverek
+        .Max(sz => sz.Ertekeles);
+    double elmarad = 0.1;
+    var megfeleloSzoftverek = szoftverek
+        .Where(sz => sz.Ertekeles + elmarad == legjobb);
+
+    foreach (var szoftver in megfeleloSzoftverek)
+    {
+        Console.WriteLine(szoftver);
+    }
+    Console.WriteLine($"\t{megfeleloSzoftverek.Count()} db szoftver van aminek az értékelés 0,1-el marad el a legjobbtól ami meg : {legjobb}\n");
+
+}
+static double AtlagBrutto(List<Szoftver> szoftverek) 
+{
+    var AdobeSzoftverek = szoftverek
+        .Where(sz => sz.Nev.StartsWith("Adobe"))
+        .ToList();
+    double osszBrutto = 0;
+
+    for (int i = 0; i < AdobeSzoftverek.Count(); i++)
+    {
+        osszBrutto += AdobeSzoftverek[i].burttoAr();
+    }
+
+    return osszBrutto / AdobeSzoftverek.Count();
+
+}
+
+static List<Szoftver> Feladat11(List<Szoftver> szoftverek) 
+{
+    return szoftverek
+        .Where(sz => sz.OpKompabilitas2 is not null)
+        .OrderBy(sz => sz.Kategoria)
+        .ToList();
+}
+static List<int> Feladat12(List<Szoftver> szoftverek) 
+{
+    return szoftverek
+        .Where(sz => sz.NettoArUSD > 500 && sz.Ertekeles < 9)
+        .Select(sz => sz.Azonosito)
+        .ToList();
+}
+static void Kiiras(List<Szoftver> szoftverek) 
+{
+    StreamWriter sw = new(@"..\..\..\src\elso10fizetos.txt",false);
+    var elso10fizetos = szoftverek
+        .Where(sz => sz.LicencTipus == "fizetős")
+        .ToList();
+    for (int i = 0; i < elso10fizetos.Count() && i < 10; i++)
+    {
+        sw.WriteLine(elso10fizetos[i].ToString());
+    }
+    sw.Close();
+}
+
+List<Szoftver> szoftverek = new();
+StreamReader sr = new(@"..\..\..\src\szoftverek.txt",Encoding.UTF8);
+
+_ = sr.ReadLine();
+
+while (!sr.EndOfStream) szoftverek.Add(new(sr.ReadLine()));
+
+foreach (var szoftver in szoftverek)
+    Console.WriteLine(szoftver);
+
+Console.WriteLine("7. feladat:\n");
+foreach (var szoftver in MagasErtekelesuAntivirus(szoftverek))
+    Console.WriteLine(szoftver);
+
+Console.WriteLine("8. feladat:\n");
+MajdnemLegjobbErtekeles(szoftverek);
+
+Console.WriteLine("10. feladat:\n");
+Console.WriteLine($"\tAz Adobe termékek átlagos bruttó ára: {AtlagBrutto(szoftverek)} usd\n");
+
+Console.WriteLine("11. feladat:\n");
+try
+{
+    foreach (var szoftver in Feladat11(szoftverek))
+    {
+        Console.WriteLine(szoftver);
+    }
+}
+catch
+{
+    Console.WriteLine("\tNincs olyan szoftver a listábna ami kompatibilis lenne 2 operációs rendszerrel!\n");
+}
+
+Console.WriteLine("12. feladat:\n");
+
+if (Feladat12(szoftverek).Count() == 0)
+    Console.WriteLine("\tNincs olyan szoftver ami 500 USD-nál drágább, 9-esnél gyengébb értékeléssel rendelkezik!\n");
+else 
+{
+    Console.WriteLine("\t500 USD-nál drágább, 9-esnél gyengébb értékeléssel rendelkező szoftverek azonosíja/ji: ");
+    foreach (var szoftverID in Feladat12(szoftverek))
+    {
+        Console.Write($"{szoftverID}");
+    }
+}
+
+Kiiras(szoftverek);
+
+Console.ReadKey();
---
+++ Szoftverek/Szoftverek/Szoftver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szoftverek
+{
+    class Szoftver
+    {
+        public double burttoAr() 
+        {
+            double szazalek = 1 + (Adokulcs / 100);
+
+            return NettoArUSD * szazalek;
+        }
+
+        public int Azonosito { get; set; }
+        public string Nev { get; set; }
+        public string LicencTipus { get; set; }
+        public string OpKompabilitasString { get; set; }
+        public string OpKompabilitas1 { get; set; }
+        public string OpKompabilitas2 { get; set; }
+        public string Kategoria { get; set; }
+        public double Ertekeles { get; set; }
+        public double NettoArUSD { get; set; }
+        public int Adokulcs { get; set; }
+
+        public Szoftver(string sor)
+        {
+            var s = sor.Split('|');
+            this.Azonosito = int.Parse(s[0]);
+            this.Nev = s[1];
+            this.LicencTipus = s[2];
+            this.OpKompabilitasString = s[3];
+            if (OpKompabilitasString.Split(',').Length == 2) 
+            {
+                var s2 = OpKompabilitasString.Split(',');
+                this.OpKompabilitas1 = s2[0];
+                this.OpKompabilitas2 = s2[1];
+            }
+            else
+                this.OpKompabilitas1 = s[3];
+            this.Kategoria = s[4];
+            this.Ertekeles = double.Parse(s[5].Replace(',','.'));
+            this.NettoArUSD = double.Parse(s[6].Replace(',','.'));
+            this.Adokulcs = int.Parse(s[7]);
+        }
+
+        public override string ToString()
+        {
+            return $"\tazonosító: {this.Azonosito} \n\tnév és verziószám: {this.Nev} \n\tlicenc típus: {this.LicencTipus} \n\toperációs rendszerek: {this.OpKompabilitasString} \n\tkategória: {this.Kategoria} \n\tfelhasználói értékelés: {this.Ertekeles} \n\tnettó ár: {this.NettoArUSD} \n\tadókulcs: {this.Adokulcs}% \n";
+        }
+    }
+}
